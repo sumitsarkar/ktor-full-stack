d67892842700ee63f899a7367824d6e526c5d8e0
Sumit Sarkar <ssarkar@cimpress.com>
2021-05-22 19:17:01 +0530

    FIX: Package imports

--- server/src/main/kotlin/me/sumit/uptime/Application.kt
+++ server/src/main/kotlin/me/sumit/uptime/Application.kt
@@ -2,7 +2,7 @@ package me.sumit.uptime
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import me.sumit.plugins.*
+import me.sumit.uptime.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
